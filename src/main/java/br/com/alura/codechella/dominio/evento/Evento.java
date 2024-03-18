f814ac73c8f7136bf6295505d8c105101babef27
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-03-18 17:51:52 -0300

    vídeo 3.2

--- src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
+++ src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
@@ -5,13 +5,28 @@ import br.com.alura.codechella.dominio.ingresso.TipoIngresso;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Evento {
+    private UUID uuid;
     private Categoria categoria;
     private String descricao;
     private Endereco endereco;
     private LocalDateTime data;
     private List<TipoIngresso> tipoIngressos = new ArrayList<>();
+
+    public Evento(Categoria categoria, String descricao, Endereco endereco, LocalDateTime data) {
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.endereco = endereco;
+        this.data = data;
+
+        gerarIdentificadorUnico();
+    }
+
+    private void gerarIdentificadorUnico() {
+        this.uuid = UUID.randomUUID();
+    }
 
     public void incluiNovoTipoDeIngressoAoEvento(TipoIngresso tipoIngresso){
         this.tipoIngressos.add(tipoIngresso);
